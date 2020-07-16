7c5f4a17257d3dafe32d654f9151bacd0036bf35
James Teitsworth <james@jamesteitsworth.com>
2020-07-15 23:20:26 -0500

    Add player sprite and wire up collision with platform

--- src/scenes/Game.js
+++ src/scenes/Game.js
@@ -8,6 +8,7 @@ export default class Game extends Phaser.Scene {
   preload() {
     this.load.image('background', 'assets/bg_layer1.png')
     this.load.image('platform', 'assets/ground_grass.png')
+    this.load.image('bunny-stand', 'assets/bunny1_stand.png')
   }
 
   create() {
@@ -27,5 +28,10 @@ export default class Game extends Phaser.Scene {
       const body = platform.body
       body.updateFromGameObject()
     }
+
+    const player = this.physics.add.sprite(240, 320, 'bunny-stand')
+      .setScale(0.5)
+
+    this.physics.add.collider(platforms, player)
   }
 } 
